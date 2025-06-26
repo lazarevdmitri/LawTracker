d62af2bfcf504aecf20288c676654543e73169ca
lazarevdmitri <lazarevd99@mail.ru>
2025-06-26 22:35:01 +0300

    Delete app directory

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(120), nullable=False)
-    content = db.Column(db.Text)
--- app/utils/diff_tool.py
+++
@@ -1,9 +0,0 @@
-import difflib
-
-def compare_texts(text1, text2):
-    return difflib.HtmlDiff().make_file(
-        text1.splitlines(),
-        text2.splitlines(),
-        fromdesc="Документ 1",
-        todesc="Документ 2"
-    )
